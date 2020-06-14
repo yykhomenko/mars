84a652c7793574366189dfdecfb8abafcb2c8784
yykhomenko <yykhomenko@gmail.com>
2020-06-14 22:05:06 +0300

    ref

--- internal/app/server/server.go
+++ internal/app/server/server.go
@@ -23,37 +23,37 @@ func messages() http.HandlerFunc {
 
 		fmt.Println(src, dst, text)
 
-		// message := &smpp.ShortMessage{
-		// 	Src:           src,
-		// 	SourceAddrTON: getTON(src),
-		// 	Dst:           dst,
-		// 	Text:          pdutext.Raw(text),
-		// 	Register:      pdufield.FinalDeliveryReceipt,
-		// }
-		//
-		// resp, e := tx.Submit(message)
-		//
-		// if e == smpp.ErrNotConnected {
-		// 	http.Error(w, "Oops.", http.StatusServiceUnavailable)
-		// 	return
-		// }
-		//
-		// if e != nil {
-		// 	http.Error(w, e.Error(), http.StatusBadRequest)
-		// 	return
-		// }
-		//
-		// midStr := resp.RespID()
-		//
-		// mid, e := strconv.ParseUint(midStr, 16, 0)
-		// if e != nil {
-		// 	log.Println("parse MID error: ", e.Error())
-		// }
-		//
-		// log.Println("mid:", mid)
-		//
-		// _, _ = io.WriteString(w, midStr)
-
 		log.Printf("duration: %s", time.Now().Sub(start))
 	}
 }
+
+// message := &smpp.ShortMessage{
+// 	Src:           src,
+// 	SourceAddrTON: getTON(src),
+// 	Dst:           dst,
+// 	Text:          pdutext.Raw(text),
+// 	Register:      pdufield.FinalDeliveryReceipt,
+// }
+//
+// resp, e := tx.Submit(message)
+//
+// if e == smpp.ErrNotConnected {
+// 	http.Error(w, "Oops.", http.StatusServiceUnavailable)
+// 	return
+// }
+//
+// if e != nil {
+// 	http.Error(w, e.Error(), http.StatusBadRequest)
+// 	return
+// }
+//
+// midStr := resp.RespID()
+//
+// mid, e := strconv.ParseUint(midStr, 16, 0)
+// if e != nil {
+// 	log.Println("parse MID error: ", e.Error())
+// }
+//
+// log.Println("mid:", mid)
+//
+// _, _ = io.WriteString(w, midStr)
